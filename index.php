91cde34044f29c49cdfb01a05c374915147b4746
Igor Emelyanov <mega.itachi.uchiha@mail.ru>
2020-07-21 20:29:51 +0300

    Настройка базовой конфигурации Doctrine

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ use Slim\Factory\AppFactory;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
-require_once __DIR__ . './vendor/autoload.php';
+require_once __DIR__ . './bootstrap.php';
 
 $app = AppFactory::create();
 
