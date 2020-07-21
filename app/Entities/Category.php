f2079c2d70138a42d005bafaa81918a335b25ff2
Igor Emelyanov <mega.itachi.uchiha@mail.ru>
2020-07-21 20:30:07 +0300

    Сущность категории

---
+++ app/Entities/Category.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class Category
+ * @package App\Entities
+ *
+ * @ORM\Entity
+ * @ORM\Table(name="categories")
+ */
+class Category {
+
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $name;
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function setName($name) {
+        $this->name = $name;
+    }
+}
