88bfafb4f77d8989bb2af328ec88d7842e617957
Igor Emelyanov <mega.itachi.uchiha@mail.ru>
2020-07-23 23:07:10 +0300

    Контроллер категорий с отображением списка и созданием записи

---
+++ app/Controllers/CategoryController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Builders\CategoryBuilder;
+use App\Entities\Category;
+use App\Wrappers\ResponseWrapper;
+use Doctrine\ORM\EntityManager;
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+class CategoryController extends BaseController {
+    protected $container;
+    protected $categoryRepository;
+
+    public function __construct(ContainerInterface $container, EntityManager $entityManager) {
+        $this->container = $container;
+        $this->categoryRepository = $entityManager->getRepository(Category::class);
+    }
+
+    public function index(ServerRequestInterface $request, ResponseInterface $response) {
+        $response = new ResponseWrapper($response);
+        $fGetList = function (Category $item) {
+            return [
+                'id' => $item->getId(),
+                'name' => $item->getName(),
+            ];
+        };
+        return $response->toJson(array_map($fGetList, $this->categoryRepository->getList()));
+    }
+
+    public function create(ServerRequestInterface $request, ResponseInterface $response) {
+        $response = new ResponseWrapper($response);
+        $dto = (new CategoryBuilder())->buildDTO($request->getQueryParams());
+        return $response->toJson($this->categoryRepository->create($dto)->toArray());
+    }
+}
