0da343396505ecafcbec4b7f935034a95d941352
Igor Emelyanov <mega.itachi.uchiha@mail.ru>
2020-07-23 21:59:44 +0300

    Базовый класс builderDTO

--- app/Builders/BaseBuilder.php
+++ app/Builders/BaseBuilder.php
@@ -5,5 +5,15 @@ namespace App\Builders;
 use App\DTO\BaseDTO;
 
 abstract class BaseBuilder {
-    abstract function buildDTO(array $params): BaseDTO;
+    private $dtoFields = [];
+    private $dtoClass = null;
+
+    public function buildDTO(array $rawParams): BaseDTO {
+        $params = [];
+        foreach ($this->dtoFields as $field) {
+            $params[] = $rawParams[$field];
+        }
+        $params[] = $rawParams['name'];
+        return new $this->dtoClass($params);
+    }
 }
