14e706a49fa97b13289a9e8e4f10be1411890dfc
Igor Emelyanov <mega.itachi.uchiha@mail.ru>
2020-07-23 23:05:06 +0300

    Правка базового билдера, чтобы записывал ключ свойства и наследники видели его свойства

--- app/Builders/BaseBuilder.php
+++ app/Builders/BaseBuilder.php
@@ -5,15 +5,14 @@ namespace App\Builders;
 use App\DTO\BaseDTO;
 
 abstract class BaseBuilder {
-    private $dtoFields = [];
-    private $dtoClass = null;
+    protected $dtoFields = [];
+    protected $dtoClass = null;
 
     public function buildDTO(array $rawParams): BaseDTO {
         $params = [];
         foreach ($this->dtoFields as $field) {
-            $params[] = $rawParams[$field];
+            $params[$field] = $rawParams[$field];
         }
-        $params[] = $rawParams['name'];
         return new $this->dtoClass($params);
     }
 }
