d273f3da4deec229126f28a24e6e88fd71271df7
Igor Emelyanov <mega.itachi.uchiha@mail.ru>
2020-07-26 23:25:41 +0300

    Перенос DI контейнера и EM в базовый класс контроллера

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -2,5 +2,15 @@
 
 namespace App\Controllers;
 
+use Doctrine\ORM\EntityManager;
+use Psr\Container\ContainerInterface;
+
 abstract class BaseController {
+    protected $container;
+    protected $entityManager;
+
+    public function __construct(ContainerInterface $container, EntityManager $entityManager) {
+        $this->container = $container;
+        $this->entityManager = $entityManager;
+    }
 }
