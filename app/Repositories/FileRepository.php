95972d1064fcff325d2eb13a3fd6fdeb5a62e8ab
Igor Emelyanov <mega.itachi.uchiha@mail.ru>
2020-07-27 01:48:02 +0300

    Репозиторий файлов

--- app/Repositories/FileRepository.php
+++ app/Repositories/FileRepository.php
@@ -2,8 +2,28 @@
 
 namespace App\Repositories;
 
+use App\Entities\File;
 use Doctrine\ORM\EntityRepository;
 
-class FileRepository extends EntityRepository {
+class FileRepository extends EntityRepository implements RepositoryInterface {
 
+    public function getList() {
+        // TODO: Implement getList() method.
+    }
+
+    public function create(array $params) {
+        $file = new File();
+        $file->setPath($params['path']);
+        $this->_em->persist($file);
+        $this->_em->flush();
+        return $file;
+    }
+
+    public function update() {
+        // TODO: Implement update() method.
+    }
+
+    public function delete() {
+        // TODO: Implement delete() method.
+    }
 }
