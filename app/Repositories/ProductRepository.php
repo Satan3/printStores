75d7b883be77b041d715cba5bb64e911b84ce2cd
Igor Emelyanov <mega.itachi.uchiha@mail.ru>
2020-08-09 23:51:57 +0300

    Репозиторий продукта

---
+++ app/Repositories/ProductRepository.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Repositories;
+
+class ProductRepository implements RepositoryInterface {
+
+    public function getList() {
+        // TODO: Implement getList() method.
+    }
+
+    public function create(array $params) {
+        // TODO: Implement create() method.
+    }
+
+    public function update(array $params) {
+        // TODO: Implement update() method.
+    }
+
+    public function delete(int $id) {
+        // TODO: Implement delete() method.
+    }
+}
