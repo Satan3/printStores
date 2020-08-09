979990fa3b8d34b0471c3251451e71f564833b04
Igor Emelyanov <mega.itachi.uchiha@mail.ru>
2020-08-10 00:01:28 +0300

    Получение товаров категории

---
+++ app/Controllers/ProductController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Entities\Category;
+use App\Entities\Product;
+use App\Wrappers\ResponseWrapper;
+use Doctrine\ORM\EntityManager;
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+class ProductController extends BaseController {
+    protected $productRepository;
+
+    public function __construct(ContainerInterface $container, EntityManager $entityManager) {
+        parent::__construct($container, $entityManager);
+        $this->productRepository = $entityManager->getRepository(Product::class);
+    }
+
+    public function index(ServerRequestInterface $request, ResponseInterface $response, int $categoryId) {
+        $response = new ResponseWrapper($response);
+        $categoryRepository = $this->entityManager->getRepository(Category::class);
+        return $response->toJson($this->toArray($categoryRepository->getProducts($categoryId)));
+    }
+}
