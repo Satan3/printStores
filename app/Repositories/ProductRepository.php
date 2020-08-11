ec6de062f9cd227a9df60d129249483f36abaf98
Igor Emelyanov <mega.itachi.uchiha@mail.ru>
2020-08-11 22:25:24 +0300

    Метод сохранения товара в репозитории

--- app/Repositories/ProductRepository.php
+++ app/Repositories/ProductRepository.php
@@ -2,14 +2,49 @@
 
 namespace App\Repositories;
 
-class ProductRepository implements RepositoryInterface {
+use App\Entities\Category;
+use App\Entities\Product;
+use App\Managers\FileManager;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Mapping;
+
+class ProductRepository extends EntityRepository implements RepositoryInterface {
+
+    protected $container;
+    protected $entityClassName = Product::class;
+    protected $path = 'products';
+    protected $fileManager;
+
+    public function __construct(EntityManager $em, Mapping\ClassMetadata $class) {
+        parent::__construct($em, $class);
+        $this->fileManager = new FileManager($this->path);
+    }
 
     public function getList() {
         // TODO: Implement getList() method.
     }
 
     public function create(array $params) {
-        // TODO: Implement create() method.
+        $product = new $this->entityClassName();
+        $file = $this->fileManager->save($params['image']);
+        /** @var Category $category */
+        if (!$category = $this->_em->getRepository(Category::class)->find($params['category_id'])) {
+            return [
+                'Указанной категории не существует'
+            ];
+        }
+        $product
+            ->setName($params['name'])
+            ->setFile($file)
+            ->setCategory($category)
+            ->setDiscount($params['discount'])
+            ->setPrice($params['price'])
+            ->setStock($params['stock'])
+        ;
+        $this->_em->persist($product);
+        $this->_em->flush();
+        return $product;
     }
 
     public function update(array $params) {
